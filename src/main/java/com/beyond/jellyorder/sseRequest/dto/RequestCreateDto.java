9e11cc7532e806d0045feea5072eb588bdd34f0a
ifunhy <guswl8296@gmail.com>
2025-08-02 00:19:30 +0900

    Feat: SSE 엔티티 및 DTO 구현 - RequestCreateDto : 고객이 점주에게 요청사항을 전송하기 위한 dto 구현 - RequestSseController : 점주가 SSE를 구독하는 dto 구현
    Refs: #11

---
+++ src/main/java/com/beyond/jellyorder/sseRequest/dto/RequestCreateDto.java
@@ -0,0 +1,20 @@
+package com.beyond.jellyorder.sseRequest.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+// 고객 -> 점주, 실시간 요청용
+public class RequestCreateDto implements Serializable {
+    private UUID id;
+    private String name;
+    private String storeId;
+    private String tableId;
+    private String tableName;
+}
