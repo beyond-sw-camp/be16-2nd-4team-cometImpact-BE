3951258829f44b6c0639bcf977aae119dc517975
ifunhy <guswl8296@gmail.com>
2025-08-04 12:15:40 +0900

    Feat: AuthRedisConfig 파일 분리

--- src/main/java/com/beyond/jellyorder/config/AuthRedisConfig.java
+++ src/main/java/com/beyond/jellyorder/config/AuthRedisConfig.java
@@ -1,9 +1,7 @@
 package com.beyond.jellyorder.config;
 
 import com.beyond.jellyorder.common.auth.RedisProperties;
-import com.beyond.jellyorder.domain.sseRequest.service.SseAlarmService;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,15 +9,15 @@ import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.listener.PatternTopic;
-import org.springframework.data.redis.listener.RedisMessageListenerContainer;
-import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.security.Key;
 
 @Configuration
 @EnableConfigurationProperties(RedisProperties.class)
 public class AuthRedisConfig {
+
     private final RedisProperties props;
 
     public AuthRedisConfig(RedisProperties props) {
@@ -42,30 +40,6 @@ public class AuthRedisConfig {
     @Bean
     @Qualifier("rtInventory")
     public RedisTemplate<String, Object> redisTemplate(@Qualifier("rtInventory") RedisConnectionFactory redisConnectionFactory) {
-        RedisTemplate<String, Object> template = new RedisTemplate<>();
-        template.setConnectionFactory(redisConnectionFactory);
-
-        // 직렬화 설정
-        template.setKeySerializer(new StringRedisSerializer());
-        template.setValueSerializer(new Jackson2JsonRedisSerializer<>(Object.class));
-        return template;
-    }
-
-    // 개수 관리 redis
-    @Bean
-    @Qualifier("stockInventory")
-    public RedisConnectionFactory stockConnectionFactory() {
-        RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration();
-        configuration.setHostName(props.getHost());
-        configuration.setPort(props.getPort());
-        configuration.setDatabase(1);
-
-        return (new LettuceConnectionFactory(configuration));
-    }
-
-    @Bean
-    @Qualifier("stockInventory")
-    public RedisTemplate<String, Object> stockTemplate(@Qualifier("stockInventory") RedisConnectionFactory redisConnectionFactory) {
         RedisTemplate<String, Object> template = new RedisTemplate<>();
         template.setConnectionFactory(redisConnectionFactory);
 
