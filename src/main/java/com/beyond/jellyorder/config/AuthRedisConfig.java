bfbd0a09adf17ad5791158a6875387ecd9dbc8de
ifunhy <guswl8296@gmail.com>
2025-08-04 16:24:52 +0900

    Fix: AuthRedisConfig 분리 및 충돌 수정

--- src/main/java/com/beyond/jellyorder/config/AuthRedisConfig.java
+++ src/main/java/com/beyond/jellyorder/config/AuthRedisConfig.java
@@ -1,51 +1,41 @@
 package com.beyond.jellyorder.config;
 
-import com.beyond.jellyorder.common.auth.RedisProperties;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
-import java.security.Key;
-
 @Configuration
-@EnableConfigurationProperties(RedisProperties.class)
 public class AuthRedisConfig {
-
-    private final RedisProperties props;
-
-    public AuthRedisConfig(RedisProperties props) {
-        this.props = props;
-    }
+    @Value("${spring.redis.host}")
+    private String host;
+    @Value("${spring.redis.port}")
+    private int port;
 
     /* RefreshToken Redis에 저장하기 위한 Factory, 0번에 설정하였으나 추후 변경 가능 합니다! */
-    @Bean
-    @Qualifier("rtInventory")
+    @Bean(name = "redisConnectionFactory")
     public RedisConnectionFactory redisConnectionFactory() {
         RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration();
-        configuration.setHostName(props.getHost());
-        configuration.setPort(props.getPort());
+        configuration.setHostName(host);
+        configuration.setPort(port);
         configuration.setDatabase(0);
 
         return new LettuceConnectionFactory(configuration);
     }
 
     /* RefreshToken RedisTemplate */
-    @Bean
-    @Qualifier("rtInventory")
-    public RedisTemplate<String, Object> redisTemplate(@Qualifier("rtInventory") RedisConnectionFactory redisConnectionFactory) {
-        RedisTemplate<String, Object> template = new RedisTemplate<>();
-        template.setConnectionFactory(redisConnectionFactory);
-
-        // 직렬화 설정
-        template.setKeySerializer(new StringRedisSerializer());
-        template.setValueSerializer(new Jackson2JsonRedisSerializer<>(Object.class));
-        return template;
+    @Bean(name = "redisTemplate")
+    public RedisTemplate<String, Object> redisTemplate(@Qualifier("redisConnectionFactory") RedisConnectionFactory redisConnectionFactory) { // 0번 팩토리 (Bean)싱글톤 객체로 주입 받겠다
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new StringRedisSerializer());
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+        return redisTemplate;
     }
+
 }
