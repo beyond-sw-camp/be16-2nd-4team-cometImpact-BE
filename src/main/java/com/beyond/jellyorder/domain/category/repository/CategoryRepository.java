ed9af7e4513f5f6f52447fcfdfbd086ca1d688d4
Hyeseong Bak <hsbak.business@gmail.com>
2025-08-05 17:41:39 +0900

    Refactor: 카테고리 조회 방식 UUID 기반으로 통합
    - 기존 findByStoreIdAndName → findByIdAndStoreId로 변경
    - UUID 기반 식별자로 일관성 있는 조회 방식 적용
    - 임시 storeId 기반 메서드 주석 처리 및 제거 예정

--- src/main/java/com/beyond/jellyorder/domain/category/repository/CategoryRepository.java
+++ src/main/java/com/beyond/jellyorder/domain/category/repository/CategoryRepository.java
@@ -9,7 +9,8 @@ import java.util.UUID;
 
 public interface CategoryRepository extends JpaRepository<Category, UUID>, CategoryRepositoryCustom{
     boolean existsByStoreIdAndName(String storeId, String name); // 임시 String storeId. **추후 UUID로 변경 필수**
-    Optional<Category> findByIdAndStoreId(UUID id, String storeId);
+//    Optional<Category> findByIdAndStoreId(UUID id, String storeId);
     Optional<Category> findByStoreIdAndName(String storeId, String name);
     List<Category> findAllByStoreId(String storeId);
+    Optional<Category> findByIdAndStoreId(UUID id, String storeId);
 }
