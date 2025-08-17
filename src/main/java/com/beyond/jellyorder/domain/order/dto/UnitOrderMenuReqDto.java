9285afe1354c79f88b5026a85548519f3e6e91a8
ifunhy <guswl8296@gmail.com>
2025-08-17 19:20:26 +0900

    Chore: 주문내역 조회 dto 수정

--- src/main/java/com/beyond/jellyorder/domain/order/dto/UnitOrderMenuReqDto.java
+++ src/main/java/com/beyond/jellyorder/domain/order/dto/UnitOrderMenuReqDto.java
@@ -1,9 +1,11 @@
 package com.beyond.jellyorder.domain.order.dto;
 
+import com.beyond.jellyorder.domain.option.dto.MainOptionReqDto;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.List;
 import java.util.UUID;
 
 /** 단위 주문 메뉴 DTO */
@@ -13,4 +15,5 @@ import java.util.UUID;
 public class UnitOrderMenuReqDto {
     private UUID menuId;
     private Integer quantity;
+    private List<MainOptionReqDto> mainOptions;
 }
