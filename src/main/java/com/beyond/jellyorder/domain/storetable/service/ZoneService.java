9891d25f154a5c68a8957da959b9cc9b0d3952f6
JeaPple <hyungkey0601@gmail.com>
2025-08-28 22:50:05 +0900

    Feat: 구역(Zone) 삭제 로직 구현 - zoneId로 삭제 요청 시 해당 구역내에 모든 테이블들 삭제(소프트 삭제) 및 해당 구역 삭제(하드 삭제) - 서비스 로직에서 진행 중인 주문이 있는 지 검증 후 삭제 - 테이블 삭제 시 FK로 걸어져 있는 store_table의 zone_id을 null값으로 update 함. Refs: #172

--- src/main/java/com/beyond/jellyorder/domain/storetable/service/ZoneService.java
+++ src/main/java/com/beyond/jellyorder/domain/storetable/service/ZoneService.java
@@ -1,5 +1,6 @@
 package com.beyond.jellyorder.domain.storetable.service;
 
+import com.beyond.jellyorder.common.auth.StoreJwtClaimUtil;
 import com.beyond.jellyorder.common.exception.DuplicateResourceException;
 import com.beyond.jellyorder.domain.store.entity.Store;
 import com.beyond.jellyorder.domain.store.repository.StoreRepository;
@@ -7,7 +8,9 @@ import com.beyond.jellyorder.domain.storetable.dto.zone.ZoneCreateReqDTO;
 import com.beyond.jellyorder.domain.storetable.dto.zone.ZoneListResDTO;
 import com.beyond.jellyorder.domain.storetable.dto.zone.ZoneResDTO;
 import com.beyond.jellyorder.domain.storetable.dto.zone.ZoneUpdateReqDTO;
+import com.beyond.jellyorder.domain.storetable.entity.StoreTable;
 import com.beyond.jellyorder.domain.storetable.entity.Zone;
+import com.beyond.jellyorder.domain.storetable.repository.StoreTableRepository;
 import com.beyond.jellyorder.domain.storetable.repository.ZoneRepository;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
@@ -27,6 +30,9 @@ public class ZoneService {
 
     private final ZoneRepository zoneRepository;
     private final StoreRepository storeRepository;
+    private final StoreJwtClaimUtil storeJwtClaimUtil;
+    private final StoreTableRepository storeTableRepository;
+
 
     // 구역 생성
     public ZoneResDTO createZone(ZoneCreateReqDTO dto) {
@@ -72,6 +78,39 @@ public class ZoneService {
 
         zone.updateName(dto.getZoneName());
         return ZoneResDTO.from(zone);
+    }
+
+    @Transactional
+    public UUID deleteZone(UUID zoneId) {
+        // storeId 추출
+        UUID storeId = UUID.fromString(storeJwtClaimUtil.getStoreId());
+
+        // 소유권/존재 검증
+        Zone zone = zoneRepository.findById(zoneId)
+                .orElseThrow(() -> new EntityNotFoundException("해당 구역이 존재하지 않습니다."));
+        if (!zone.getStore().getId().equals(storeId)) {
+            throw new IllegalArgumentException("다른 매장의 구역을 삭제할 수 없습니다.");
+        }
+
+        // 해당 구역에 대한 storeTable 전부 삭제(soft)
+        List<StoreTable> storeTableList = storeTableRepository.findAllByZone(zone);
+        for (StoreTable storeTable : storeTableList) {
+            // 진행 중인 주문있는지 검증
+            if (storeTableRepository.existsOpenOrder(storeTable.getId())) {
+                throw new IllegalArgumentException("진행 중 주문이 있어 비활성화할 수 없습니다.");
+            }
+
+            storeTableRepository.delete(storeTable);
+        }
+
+        // 테이블의 FK 끊기 (zone_id = null)
+        int i = storeTableRepository.detachSoftDeletedFromZone(zoneId);
+        System.out.println("i = " + i);
+
+        // 구역 삭제(hard)
+        zoneRepository.delete(zone);
+
+        return zone.getId();
     }
 
 
