71490b3e2b42b1bb8ab8e6826fe3a1ec6bf91861
JeaPple <hyungkey0601@gmail.com>
2025-09-01 14:03:14 +0900

    Feat: 테이블 비우기 API 구현

--- src/main/java/com/beyond/jellyorder/domain/order/repository/TotalOrderRepository.java
+++ src/main/java/com/beyond/jellyorder/domain/order/repository/TotalOrderRepository.java
@@ -1,5 +1,6 @@
 package com.beyond.jellyorder.domain.order.repository;
 
+import com.beyond.jellyorder.domain.order.entity.OrderStatus;
 import com.beyond.jellyorder.domain.order.entity.TotalOrder;
 import com.beyond.jellyorder.domain.storetable.entity.StoreTable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,5 +11,9 @@ import java.util.UUID;
 public interface TotalOrderRepository extends JpaRepository<TotalOrder, UUID> {
 
     Optional<TotalOrder> findTopByStoreTableOrderByOrderedAtDesc(StoreTable table);
+
+    // "활성 주문(미종료)만" 가져오기
+    Optional<TotalOrder> findTopByStoreTableAndEndedAtIsNullOrderByOrderedAtDesc(StoreTable table);
+
 
 }
